d130bc6785415edb5bae37c856d171ba18b103ef
ehsia-hsia <erica.hsia1@gmail.com>
2021-02-19 15:48:05 -0500

    used bootstrap button group for color sort buttons, adding styling next

--- indexFlag.js
+++ indexFlag.js
@@ -21,7 +21,6 @@ function flagSearch(){
         let input = document.getElementById('myRedButton');
         // input = input.toLowerCase();
         let card = document.getElementsByClassName('card');
-        let name = document.getElementsByClassName('name');
     
             for (let i = 0; i < card.length; i++){
                 if(!card[i].innerHTML.includes("RED")){
