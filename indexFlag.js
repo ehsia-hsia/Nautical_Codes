a21a5a8995863e6e3104dbe91921729f9a842b8b
ehsia-hsia <erica.hsia1@gmail.com>
2021-02-19 15:19:54 -0500

    Added color buttons that sort cards by color. Want to put in one function with eventlisteners

--- indexFlag.js
+++ indexFlag.js
@@ -17,3 +17,83 @@ function flagSearch(){
     }
 
 
+    function Red(){
+        let input = document.getElementById('myRedButton');
+        // input = input.toLowerCase();
+        let card = document.getElementsByClassName('card');
+        let name = document.getElementsByClassName('name');
+    
+            for (let i = 0; i < card.length; i++){
+                if(!card[i].innerHTML.includes("RED")){
+                    card[i].style.display = "none";
+                } else {
+                    card[i].style.display ="block";
+                }
+            }
+            
+        }
+
+        function Blue(){
+            let input = document.getElementById('myBlueButton');
+            // input = input.toLowerCase();
+            let card = document.getElementsByClassName('card');
+            let name = document.getElementsByClassName('name');
+        
+                for (let i = 0; i < card.length; i++){
+                    if(!card[i].innerHTML.includes("BLUE")){
+                        card[i].style.display = "none";
+                    } else {
+                        card[i].style.display ="block";
+                    }
+                }
+                
+            }
+
+
+            function Yellow(){
+                let input = document.getElementById('myYellowButton');
+                // input = input.toLowerCase();
+                let card = document.getElementsByClassName('card');
+                let name = document.getElementsByClassName('name');
+            
+                    for (let i = 0; i < card.length; i++){
+                        if(!card[i].innerHTML.includes("YELLOW")){
+                            card[i].style.display = "none";
+                        } else {
+                            card[i].style.display ="block";
+                        }
+                    }
+                    
+                }
+
+                function White(){
+                    let input = document.getElementById('myWhiteButton');
+                    // input = input.toLowerCase();
+                    let card = document.getElementsByClassName('card');
+                    let name = document.getElementsByClassName('name');
+                
+                        for (let i = 0; i < card.length; i++){
+                            if(!card[i].innerHTML.includes("WHITE")){
+                                card[i].style.display = "none";
+                            } else {
+                                card[i].style.display ="block";
+                            }
+                        }
+                        
+                    }
+
+                    function Black(){
+                        let input = document.getElementById('myBlackButton');
+                        // input = input.toLowerCase();
+                        let card = document.getElementsByClassName('card');
+                        let name = document.getElementsByClassName('name');
+                    
+                            for (let i = 0; i < card.length; i++){
+                                if(!card[i].innerHTML.includes("BLACK")){
+                                    card[i].style.display = "none";
+                                } else {
+                                    card[i].style.display ="block";
+                                }
+                            }
+                            
+                        }
