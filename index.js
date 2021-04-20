69b49a0b9585d232ff6cd67ece38cd5e87d4a938
ehsia-hsia <erica.hsia1@gmail.com>
2021-04-20 17:22:35 -0400

    All background Text dark mode

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const body = document.querySelector("body");
 const header = document.querySelector(".header");
 const headerText = document.querySelector(".big-heading");
 const headerPara = document.querySelector(".aboutpara").firstElementChild;
+const morseFlex = document.getElementById("mor");
+const flagFlex = document.getElementById("flags");
+
+const flexTitle = document.querySelectorAll(".sectiontitle");
+const flexPara = document.querySelectorAll(".descriptionFlex");
+
 const buttons = document.querySelectorAll("button");
 buttons.forEach((button) => {
   button.classList.add("noButtonBorder");
@@ -12,12 +18,20 @@ const toggleSwitch = document.getElementById("customSwitch1");
 toggleSwitch.addEventListener("click", darkMode);
 function darkMode() {
   header.classList.toggle("backgroundDarkMode");
+  body.classList.toggle("backgroundDarkMode");
+  morseFlex.classList.toggle("backgroundDarkMode");
+  flagFlex.classList.toggle("backgroundDarkMode");
   headerText.classList.toggle("textDarkMode");
   headerPara.classList.toggle("textDarkMode");
-  body.classList.toggle("backgroundDarkMode");
+  flexTitle.forEach((title) => {
+    title.classList.toggle("textDarkMode");
+  });
+
+  flexPara.forEach((title) => {
+    title.classList.toggle("textDarkMode");
+  });
   buttons.forEach((button) => {
     button.classList.toggle("noButtonBorder");
-
     button.classList.toggle("borderDarkMode");
   });
 }
